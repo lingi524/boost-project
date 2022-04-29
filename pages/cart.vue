198c64b1aa7d189e9d76ab9fc9750d01e6695ea6
lingi524 <linnea.gistrand@gmail.com>
2022-04-29 12:12:48 +0200

    Add link to product from cart

--- pages/cart.vue
+++ pages/cart.vue
@@ -10,7 +10,9 @@ const { cart } = useCart();
         <div v-else v-for="(product, index) in cart" :product="product" :key="index">
             <div class="flex flex-row justify-center mx-auto mt-10 ">
                 <div class="flex flex-col p-5 border rounded-lg shadow-md">
+                    <NuxtLink :to="`/product/${product.id}`">
                     <img :src="product.image" width="300" height="300" :alt="product.name" class="object-cover mb-3 h-52" loading="lazy" style="width: 10rem">
+                   </NuxtLink>
                     <div>
                         <p class="font-bold">Produkt: {{product.name}}</p>
                         <p>Antal: {{product.quantity}}</p>
